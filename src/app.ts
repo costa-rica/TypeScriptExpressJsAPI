633e26d29055f7f6866086a727d670205770c412
Nick Rodriguez <nrodrig1@gmail.com>
2025-09-09 08:30:28 -0700

    added dotenv and .env

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-import type { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
